fa968e26d7ee8e5a0f92ca47f2871af49c71a403
fnascimento85 <f.nascimento@swordhealth.com>
2022-04-20 15:12:24 +0100

    Added create employee form

--- src/views/EmployeeCreate.vue
+++ src/views/EmployeeCreate.vue
@@ -1,15 +1,62 @@
 <template>
   <div>
       <h1>Create Employee</h1>
+       <div class="create-container">
+           <form>
+               <div class="row">
+                   <input type="text" class="form-control" placeholder="Name" v-model="employee.name" />
+               </div>
+               <div class="row">
+                   <input type="text" class="form-control" placeholder="Salary" v-model="employee.salary" />
+               </div>
+               <div class="row">
+                   <input type="text" class="form-control" placeholder="Age" v-model="employee.age" />
+               </div>
+               <div class="row">
+                   <button type="button" class="btn btn-success" @click="save()">Create</button>
+               </div>
+           </form>
+      </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
-    name: 'EmployeeCreate'
+    name: 'EmployeeCreate',
+    data: function() {
+        return {
+            employee:{
+                name:'',
+                age:'',
+                salary:''
+            }
+        };
+    },
+    methods: {
+        ...mapActions(['createEmployee']),
+        save: function() {
+            this.createEmployee(this.employee);
+            this.resetEmployee();
+        },
+        resetEmployee: function() {
+            this.employee = {
+                id: 0,
+                name:'',
+                age:'',
+                salary:''
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
+.create-container {
+  font-size: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: left;
+}
 </style>
