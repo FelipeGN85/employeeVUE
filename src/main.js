3add36e866517cf68b6024c6175ebfa5999834b1
fnascimento85 <f.nascimento@swordhealth.com>
2022-04-20 15:28:44 +0100

    Add vuelidate

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import store from './store/index'
 import router from './router'
 
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
+
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  router,
-  render: h => h(App)
+    store,
+    router,
+    render: h => h(App)
 }).$mount('#app')
