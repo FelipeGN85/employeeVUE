275eeff3447dcac284071cb5ef550e32800169dd
fnascimento85 <f.nascimento@swordhealth.com>
2022-04-21 09:19:56 +0100

    Added form to view

--- src/views/EmployeeCreate.vue
+++ src/views/EmployeeCreate.vue
@@ -1,53 +1,18 @@
 <template>
   <div>
       <h1>Create Employee</h1>
-       <div class="create-container">
-           <form>
-               <div class="row">
-                   <input type="text" class="form-control" placeholder="Name" v-model="employee.name" />
-               </div>
-               <div class="row">
-                   <input type="text" class="form-control" placeholder="Salary" v-model="employee.salary" />
-               </div>
-               <div class="row">
-                   <input type="text" class="form-control" placeholder="Age" v-model="employee.age" />
-               </div>
-               <div class="row">
-                   <button type="button" class="btn btn-success" @click="save()">Create</button>
-               </div>
-           </form>
-      </div>
+        <CreateEmployeeForm />
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import CreateEmployeeForm from '@/components/forms/CreateEmployeeForm.vue';
 export default {
     name: 'EmployeeCreate',
-    data: function() {
-        return {
-            employee:{
-                name:'',
-                age:'',
-                salary:''
-            }
-        };
+    components:{
+        CreateEmployeeForm
     },
-    methods: {
-        ...mapActions(['createEmployee']),
-        save: function() {
-            this.createEmployee(this.employee);
-            this.resetEmployee();
-        },
-        resetEmployee: function() {
-            this.employee = {
-                id: 0,
-                name:'',
-                age:'',
-                salary:''
-            }
-        }
-    }
+    
 }
 </script>
 
