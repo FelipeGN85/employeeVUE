839dc0fc77a2c7e0aa540fd40885f08276b06a0a
fnascimento85 <f.nascimento@swordhealth.com>
2022-04-21 15:03:27 +0100

    Added bootstrap table and buttons

--- src/components/List.vue
+++ src/components/List.vue
@@ -4,22 +4,28 @@
       <Spinner />
     </div>
     <div v-else-if="allEmployees.length > 0" class="container">
-      <table>
-        <tr>
-          <td>Name</td>
-          <td>Salary</td>
-          <td>Age</td>
-          <td>Details</td>
-          <td>Delete</td>
-        </tr>
-        <tr v-for="emp in allEmployees" v-bind:key="emp.id">
-          <td>{{ emp.employee_name }}</td>
-          <td>{{ emp.employee_salary }}</td>
-          <td>{{ emp.employee_age }}</td>
-          <td><button v-on:click="goToDetails(emp.id)">Details</button></td>
-          <td><button v-on:click="deleteEmp(emp.id)">Delete</button></td>
-        </tr>
-      </table>
+      <div class="wrapper-scroll-y scrollbar">
+        <table class="table table-success table-hover table-sm">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Salary</th>
+              <th scope="col">Age</th>
+              <th scope="col">Details</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="emp in allEmployees" v-bind:key="emp.id">
+              <td>{{ emp.employee_name }}</td>
+            <td>{{ emp.employee_salary }}</td>
+            <td>{{ emp.employee_age }}</td>
+            <td><button type="button" class="btn btn-success" v-on:click="goToDetails(emp.id)">Details</button></td>
+            <td><button type="button" class="btn btn-danger" v-on:click="deleteEmp(emp.id)">Delete</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -35,19 +41,18 @@ export default {
   },
   methods: {
     ...mapActions(["fetchEmployees", "deleteEmployee"]),
-    deleteEmp: function(id){
+    deleteEmp: function (id) {
       this.deleteEmployee(id);
     },
     goToDetails(id) {
       this.$router.push({
-       path:`/employee/details/${id}`,
+        path: `/employee/details/${id}`,
       });
     },
-
   },
   computed: {
     ...mapGetters(["allEmployees"]),
-    ...mapGetters(["isLoading"])
+    ...mapGetters(["isLoading"]),
   },
   created() {
     this.fetchEmployees();
@@ -62,9 +67,12 @@ export default {
   align-items: center;
   justify-content: center;
 }
-table,
-td {
-  border: 1px solid;
-  width: 35%;
+.scrollbar {
+position: relative;
+height: 825px;
+overflow: auto;
+}
+.wrapper-scroll-y {
+display: block;
 }
 </style>
