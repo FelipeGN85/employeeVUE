7ae53d18c52cf973059aa429c6d7c0658c46015f
fnascimento85 <f.nascimento@swordhealth.com>
2022-04-22 09:41:34 +0100

    Added required validations to fields

--- src/components/forms/CreateEmployeeForm.vue
+++ src/components/forms/CreateEmployeeForm.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="form-container">
       <form>
-        <input type="text" class="form-control" placeholder="Name" v-model="employee.name"/>
-        <input type="text" class="form-control mt-4" placeholder="Salary" v-model="employee.salary"/>
-        <input type="text" class="form-control mt-4" placeholder="age" v-model="employee.age"/>
-        <button type="button" class="btn btn-success mt-4" @click="save()">Create</button>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Name" v-model="employee.name"/>
+          <div class="error" v-if="!$v.employee.name.required && $v.$dirty">Name is required</div>
+          <div class="error" v-if="!$v.employee.name.minLength">Name must have at least {{$v.employee.name.$params.minLength.min}} letters.</div>
+        </div>
+        <div>
+          <input type="text" class="form-control mt-5" placeholder="Salary" v-model="employee.salary"/>
+          <div class="error" v-if="!$v.employee.salary.required && $v.$dirty">Salary is required</div>
+        </div>
+        <div>
+          <input type="text" class="form-control mt-5" placeholder="age" v-model="employee.age"/>
+          <div class="error" v-if="!$v.employee.age.required && $v.$dirty">Age is required</div>
+        </div>
+        <button type="button" class="btn btn-success mt-5" @click="save()" :disabled="submitStatus === 'PENDING'">Create</button>
+        <p class="typo__p" v-if="submitStatus === 'OK'">Thanks for your submission!</p>
+        <p class="typo__p" v-if="submitStatus === 'ERROR'">Please fill the form correctly.</p>
+        <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
       </form>
   </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
+import { required, minLength } from 'vuelidate/lib/validators'
 
 export default {
   name: "CreateEmployeeForm",
@@ -21,13 +35,38 @@ export default {
         salary: "",
         age: "",
       },
+      submitStatus: null
     };
+  },
+  validations: {
+    employee:{
+      name: {
+      required,
+      minLength: minLength(4)
+      },
+      salary:{
+        required
+      },
+      age:{
+        required
+      }
+    }
+    
   },
   methods: {
     ...mapActions(["createEmployee"]),
     save: function () {
-      this.createEmployee(this.employee);
-      this.resetEmployee();
+      this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+      } else {
+        this.createEmployee(this.employee);
+        this.resetEmployee();
+        this.submitStatus = 'PENDING'
+        setTimeout(() => {
+          this.submitStatus = 'OK'
+        }, 500)
+      }
     },
     resetEmployee: function () {
       this.employee = {
@@ -46,6 +85,6 @@ export default {
   align-items: center;
   justify-content: center;
   margin-top: 50px;
-  font-size: 30px;
+  font-size: 15px;
 }
 </style>
